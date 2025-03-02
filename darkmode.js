631d37a651ddf2b5da86408f1f1a3ea706776dd1
Abdullah Ifikhar <ai868419@gmail.com>
2025-03-02 20:42:52 +0500

    dark mode addition incomplete

---
+++ darkmode.js
@@ -0,0 +1,5 @@
+function toggleTheme() {
+  document.body.classList.toggle('dark-theme');
+  document.getElementById('iconLight').classList.toggle('d-none');
+  document.getElementById('iconDark').classList.toggle('d-none');
+}
